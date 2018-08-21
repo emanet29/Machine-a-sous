e26a433376fb69fb177a1a1e43eb05643cfd5154
Snoopy <snoopy@MacBook-Pro-de-SERGENT.local>
2018-08-21 16:06:35 +0200

    picker

--- Machine a sous/ViewController.swift
+++ Machine a sous/ViewController.swift
@@ -8,14 +8,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    
+    
     @IBOutlet weak var picker: UIPickerView!
     @IBOutlet weak var label: UILabel!
     @IBOutlet weak var bouton: UIButton!
     
     
+    var emojis = ["🥝","🍌","🍎"]
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        picker.delegate = self
+        picker.dataSource = self
+        picker.isUserInteractionEnabled = false
+        
         
         bouton.layer.cornerRadius = 30
         bouton.layer.shadowColor = UIColor.darkGray.cgColor
@@ -23,8 +35,37 @@ class ViewController: UIViewController {
         bouton.layer.shadowRadius = 4
         bouton.layer.shadowOpacity = 0.8
     }
-
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 3
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return emojis.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return emojis[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
+        return 150
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
+        let label = UILabel()
+        label.text = emojis[row]
+        label.font = UIFont.systemFont(ofSize: 80)
+        label.adjustsFontSizeToFitWidth = true
+        label.textAlignment = .center
+        
+        return label
+    }
+    
     @IBAction func boutonAction(_ sender: Any) {
+        for x in (0..<picker.numberOfComponents) {
+            picker.selectRow(Int(arc4random_uniform(UInt32(emojis.count))), inComponent: x, animated: true)
+        }
         
     }
     
